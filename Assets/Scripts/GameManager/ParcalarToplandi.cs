c5a928ab434850f740e8176da854878ab435a728
Cebrail~ <cebrail00100@gmail.com>
2023-02-18 11:11:38 +0300

    ++

---
+++ Assets/Scripts/GameManager/ParcalarToplandi.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ParcalarToplandi : MonoBehaviour
+{
+    [SerializeField] private GameObject[] taskCompleteTrigger; // aktif olacak parçalar
+    [SerializeField] private GameObject congratulationsPanel;  //oyun bitiþ panel
+    void Update()
+    {
+        CheckTasks();
+    }
+    void CheckTasks()
+    {
+        StartCoroutine(WaitForCompletion());
+    }
+    IEnumerator WaitForCompletion()
+    {
+        for (int i = 0; i < taskCompleteTrigger.Length; i++)
+        {
+            if (taskCompleteTrigger.All(x => x.activeInHierarchy)) //tüm parçalar aktifse oyun biter ve kazanýr
+            {
+                yield return new WaitForSeconds(1);
+                congratulationsPanel.SetActive(true);
+                yield return new WaitForSeconds(1);
+                GoToLevel("Puzzle");
+            }
+        }
+    }
+    private  void GoToLevel(string levelSceneName)
+    {
+        SceneManager.LoadScene(levelSceneName);
+    }
+}
